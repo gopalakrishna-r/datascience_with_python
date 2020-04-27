7f2aa4f177651465b958c034e05805ed8d5ed11e
* <gopalakrishnar91@gmail.com>
2020-04-27 21:48:40 +0530

    added friends of friend function

--- no_of_friends.py
+++ no_of_friends.py
@@ -1,3 +1,5 @@
+from collections import Counter
+
 users = [{ "id" : 0, "name" : "Hero"},
 	{"id" : 1, "name" : "Dunn"},
 	{"id" : 2  ,"name" : "Sue"},
@@ -24,5 +26,28 @@ def number_of_friends(user):
 total_connections = sum(number_of_friends(user) for user in users)
 no_of_user = len(users)
 avg_conn = total_connections/no_of_user
-num_friends_by_id = [(user["id"], number_of_friends(user) for user in users)]
+
 print(avg_conn)
+
+num_friends_by_id = [(user["id"], number_of_friends(user)) for user in users]  #loop and create pairs of id and num of friends
+num_friends_by_id.sort(key = lambda id_of_friends: id_of_friends[1],reverse= True) #sort based on based on number of friends
+
+def foaf_id_bad(user):
+	""" find the connections of the user and then their connections """
+	return [foaf_id 
+				for friend_id in friendships[user["id"]]
+				for foaf_id in friendships[friend_id]]
+				
+print(foaf_id_bad(users[0]))
+
+def friends_of_friends(user):
+	user_id = user["id"]
+	return Counter(
+	foaf_id
+	for friend_id in friendships[user_id]
+	for foaf_id in friendships[friend_id]
+	if foaf_id != user_id 
+	and foaf_id not in friendships[user_id]
+	)
+	
+print(friends_of_friends(users[3]))	
